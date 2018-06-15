0f9ed60f786a44fb43a19521a26314b09b8d9e0a
Janis Berzins <janis.berzins@hmcts.net>
2018-06-15 08:55:20 +0100

    Cleaned up groovy code

--- jenkins-pipelines/bootstrap.groovy
+++ jenkins-pipelines/bootstrap.groovy
@@ -65,8 +65,6 @@ EOF
             # Execute ansible-playbook
             ln -s roles/bootstrap-role/run_bootstrap.yml run_bootstrap.yml
             if [ "$HOSTNAME_PARAM" == "" ]; then
-
-              ansible-config view   
               ansible-playbook -i  inventory/azure_rm.py --tags "$ANSIBLE_TAGS" run_bootstrap.yml 
             else
               ansible-playbook -i "$HOSTNAME_PARAM," --tags "$ANSIBLE_TAGS" run_bootstrap.yml
